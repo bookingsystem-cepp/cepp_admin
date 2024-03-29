56e482ea22181259861aac4fda7f69869aae2867
Krit Tangpingyoputtikhun <jinokrit@gmail.com>
2024-03-29 09:11:04 +0700

    please work

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,14 +2,24 @@ import clientPromise from '@/lib/mongodb'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import axios from 'axios';
 
 const adminEmails = ['jinokrit@gmail.com', '64010022@kmitl.ac.th','64010001@kmitl.ac.th'];
 
 const DEFAULT_SCORES = 100;
 const DEFAULT_YEAR = new Date().getFullYear();
 
-const authOption = {
+export default NextAuth(authConfig)
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authConfig);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
+
+const authConfig = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -17,8 +27,9 @@ const authOption = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    session: async ({session, token, user}) => {
+    session: async ({ session, token, user }) => {
       if (adminEmails.includes(session?.user?.email)) {
         if (user) {
           // If user is present, update user data with default values
@@ -31,28 +42,22 @@ const authOption = {
             year: DEFAULT_YEAR,
             scores: DEFAULT_SCORES,
           };
-          
+
           // Update user data in the database
           await MongoDBAdapter(clientPromise).updateUser(updatedUser);
-  
-          // Return updated session access_token: res.data.access_token
-          return { ...session, user: updatedUser, token };
+
+          // Return updated session
+          return { ...session, user: updatedUser };
         }
         return session;
       } else {
         return false;
       }
     },
-  }
-}
-
-export default NextAuth(authOption)
-
-export async function isAdminRequest(req, res) {
-  const session = await getServerSession(req, res, authOption);
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401);
-    res.end();
-    throw 'not an admin';
-  }
-}
+  },
+  session: {
+    jwt: true,
+  },
+  // Define NEXTAUTH_URL environment variable
+  site: process.env.NEXTAUTH_URL || 'http://localhost:3000', // Change the default URL here
+};
